78171a37b1042f8b45f052979a646190b18f5199
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-19 11:39:31 -0700

    Updated home page ui.

--- DemoShop/urls.py
+++ DemoShop/urls.py
@@ -20,6 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('apply/', include('coupons.urls', namespace='coupons')),
+    path('cart/', include('cart.urls', namespace='cart')),
+    # path('orders/', include('orders.urls', namespace='orders')),
+    # path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop'))
 ]
 
--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.product_list, name='product-list'),
-    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('products/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
