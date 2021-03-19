6b1afd7e12df00a269373efcbfaee9a34860fd03
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-19 10:24:39 -0700

    Created coupons, orders, and payment apps.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+# from coupons.forms import CouponApplyForm
+
+
+def cart_detail(request):
+    """ Get current cart. """
+    cart = Cart(request)
+    # provide form to edit order quantity.
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(
+            initial={
+                'quantity': item['quantity'],
+                'override': True,
+            }
+        )
+    coupon_apply_form = CouponApplyForm()
+
+    context = {
+        'cart': cart,
+        'coupon_apply_form': coupon_apply_form
+    }
+
+    return render(request, 'cart/detail.html', context)
+
+
+@require_POST
+def cart_add(request, product_id):
+    """ Add product to cart. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """ Remove product from cart. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
