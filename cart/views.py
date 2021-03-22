eb70072914f34430eff6f98e4d95f301fa21c2a2
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-21 22:25:03 -0700

    Added view products list.

--- cart/views.py
+++ cart/views.py
@@ -59,16 +59,17 @@ def cart_add(request, product_id):
     }
     return render(request, 'shop/product/detail.html', context=context)
 
-# @require_POST
-# def cart_add(request, product_id):
-#     """ Add product to cart. """
-#     cart = Cart(request)
-#     product = get_object_or_404(Product, id=product_id)
-#     form = CartAddProductForm(request.POST)
-#     if form.is_valid():
-#         cd = form.cleaned_data
-#         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
-#     return redirect('cart:cart_detail')
+
+@require_POST
+def cart_update(request, product_id):
+    """ Update quantity of cart. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
 
 
 @require_POST
