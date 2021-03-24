ab64eb5b2fd902c5965bfbf39710db0589be2828
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-24 15:01:18 -0700

    Updated ui for entering credit card details.

--- orders/forms.py
+++ orders/forms.py
@@ -12,3 +12,32 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code',]
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'First Name',
+                'required': True
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Last Name',
+                'required': True
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Email',
+                'required': True
+            }),
+            'address': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Mailing Address',
+                'required': True
+            }),
+            'postal_code': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Postal Code (e.g. A1A 1A1)',
+                'required': True
+            }),
+        }
