22f477b47d04e4b67e1d61a84c5df7fa87921305
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-30 10:12:42 -0700

    Final update

--- payment/tasks.py
+++ payment/tasks.py
@@ -13,11 +13,12 @@ def payment_completed(order_id):
     """ Send email when order is successfully created."""
     order = Order.objects.get(id=order_id)
 
-    subject = f'Dem0Shop - Order no. {order.id} confirmed'
+    subject = f'DemoShop - Order no. {order.id} confirmed'
     message = f'Dear {order.first_name}, \n\n' \
               f'This email confirms your order with ID {order.id} has been ' \
-              f'received and paid for. Expect a follow-up email in a few days ' \
-              f'to notify you when your order is sent to the address provided. \n\n' \
-              f'Thank you for your business!\n'
+              f'received and paid for (in the demo sense.) Expect a follow-up email in a few days ' \
+              f'to notify you when your order is sent to the address provided (is how this email would continue if you '\
+              f'actually purchased anything, which you haven\'t.) \n\n' \
+              f'Despite this thank you for your business/interest!\n'
     email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
     email.send()
