7e952316b8cf11cf50092051dc016ad8433c333d
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-03-30 13:42:43 -0700

    Added email

--- DemoShop/settings/base.py
+++ DemoShop/settings/base.py
@@ -157,3 +157,12 @@ BRAINTREE_CONF = braintree.Configuration(
 # === email backend ===
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = pirkeys.EMAIL_HOST_USER
+    EMAIL_HOST_PASSWORD = pirkeys.EMAIL_HOST_PASSWORD
+
+    DEFAULT_FROM_EMAIL = pirkeys.EMAIL_HOST_USER
